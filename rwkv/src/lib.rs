880f1106297bac84ca0bb3b914c0e722cdca3441
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2023-05-07 20:49:24 +0100

    wip

--- rwkv/src/lib.rs
+++ rwkv/src/lib.rs
@@ -1,8 +1,9 @@
-// See https://github.com/saharNooby/rwkv.cpp/blob/master/rwkv/rwkv_cpp_shared_library.py
+// See https://github.com/saharNooby/rwkv.cpp/blob/master/rwkv/rwkv_cpp_model.py
 
 use std::{
   ffi::{c_char, CString},
   path::Path,
+  ptr,
 };
 
 use rwkv_sys;
@@ -23,7 +24,17 @@ pub enum RWKVError {
 
 pub struct RWKVModel {
   ctx: *mut rwkv_sys::rwkv_context,
+  current_state: Option<Vec<f32>>,
   tokenizer: Tokenizer,
+
+  state_buffer_element_count: usize, //= self._library.rwkv_get_state_buffer_element_count(self._ctx)
+  logits_buffer_element_count: usize, //= self._library.rwkv_get_logits_buffer_element_count(self._ctx)
+}
+
+impl Drop for RWKVModel {
+  fn drop(&mut self) {
+    unsafe { rwkv_sys::rwkv_free(self.ctx) }
+  }
 }
 
 impl RWKVModel {
@@ -46,20 +57,43 @@ impl RWKVModel {
     let tokenizer =
       Tokenizer::from_file(tokens_path).map_err(|e| RWKVError::TokenReadFailure { source: e })?;
 
+    let state_size = unsafe { rwkv_sys::rwkv_get_state_buffer_element_count(ctx) } as usize;
+    let logits_size = unsafe { rwkv_sys::rwkv_get_logits_buffer_element_count(ctx) } as usize;
+
     Ok(RWKVModel {
       ctx: ctx,
       tokenizer: tokenizer,
+      current_state: None,
+      state_buffer_element_count: state_size,
+      logits_buffer_element_count: logits_size,
     })
   }
 
-  pub fn predict<'s, E>(self, text: &str) -> Result<&str, RWKVError> {
-    let encoding = self.tokenizer.encode(text, true).map_err(|e| RWKVError::TokenEncodeError { source:  e })?;
+  pub fn predict(self, text: &str) -> Result<&str, RWKVError> {
+    let encoding = self
+      .tokenizer
+      .encode(text, true)
+      .map_err(|e| RWKVError::TokenEncodeError { source: e })?;
+
+    let mut next_state: Vec<f32> = vec![0.0f32; self.state_buffer_element_count];
+    let mut next_logits: Vec<f32> = vec![0.0f32; self.logits_buffer_element_count];
 
     for token in encoding.get_ids() {
-      // rwkv_sys::rwkv_eval(self.ctx,token, ...)
+      let state_arg = match &self.current_state {
+        None => ptr::null_mut(),
+        Some(state) => state.clone().as_mut_ptr(),
+      };
+
+      let res = unsafe {
+        rwkv_sys::rwkv_eval(
+          self.ctx,
+          *token as i32, // this should be OK - if not, hilarity ensues
+          state_arg,
+          next_state.as_mut_ptr(),
+          next_logits.as_mut_ptr(),
+        )
+      };
     }
-
-
 
     Ok("hi")
   }
